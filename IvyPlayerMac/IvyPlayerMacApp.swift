36f3af3b543b4205f7fed95c1ceaaa8436a38b46
Ivan Almada <ivan.almada@me.com>
2025-02-13 21:47:14 -0600

    Initial commit

---
+++ IvyPlayerMac/IvyPlayerMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  IvyPlayerMacApp.swift
+//  IvyPlayerMac
+//
+//  Created by Ivan Almada on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct IvyPlayerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
